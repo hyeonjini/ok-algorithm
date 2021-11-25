54705775e12b860203415bed2f8fe4d5e75f62af
hyeonjin <choihj94@gmail.com>
2021-11-26 01:18:44 +0900

    solve boj

---
+++ BOJ/chj/1339.py
@@ -0,0 +1,31 @@
+"""
+단어 수학
+"""
+
+import sys
+
+input = sys.stdin.readline
+max_length = 10
+min_length = 1
+
+def solution(n, words):
+    answer = 0
+    allocate = {}
+
+    for word in words:
+        pass
+    
+    # allocate number
+    # input words
+
+    return answer
+def get_next_index(word):
+    return 0
+if __name__ == "__main__":
+    n = int(input())
+    words = []
+
+    for _ in range(n):
+        words.append(input().rstrip())
+    
+    print(solution(n, words))
