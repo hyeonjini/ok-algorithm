6ff15b505b8667ffb1163cfc0ff1e77d396bb215
Syon0303 <syon03@gmail.com>
2021-11-26 10:58:18 +0900

    solved 5430

--- BOJ/ksh/5430.py
+++ BOJ/ksh/5430.py
@@ -1,9 +1,3 @@
-'''
-AC
-'''
-
-# 나의 풀이
-
 import sys
 from collections import deque
 
@@ -11,9 +5,33 @@ t = int(input())
 
 for i in range(t):
     p = sys.stdin.readline().rstrip()
-    print(p)
     n = int(input())
 
-    arr = deque(sys.stdin.readline().rstrip().split(","))
-    print(arr[1])
+    arr = deque(sys.stdin.readline().rstrip()[1:-1].split(","))
 
+    okPrint = True
+    revCount = 0
+
+    if n == 0:
+        arr = []
+
+    for j in p:
+        if j == 'R':
+            revCount = revCount + 1
+        elif j == 'D':
+            if len(arr) >= 1:
+                if revCount % 2 == 0:
+                    arr.popleft()
+                else:
+                    arr.pop()
+            else:
+                print("error")
+                okPrint = False
+                break
+
+    if okPrint == True:
+        if revCount % 2 == 0:
+            print("[" + ",".join(arr) + "]")
+        else:
+            arr.reverse()
+            print("[" + ",".join(arr) + "]")
